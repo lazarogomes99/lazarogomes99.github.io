03850a1413ed1e032378e10ce7fe5707f866ced5
lazarogomes99 <lazarogomes99@gmail.com>
2023-07-27 01:17:35 -0300

    :tada: Primeiro commit do meu Resumo-Site

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+const titulo = document.querySelector('h4')
+const tituloH1 = document.querySelector('h1')
+
+function digitando(elemento) {
+    const textoArray = elemento.innerHTML.split('')
+    elemento.innerHTML = ''
+    textoArray.forEach((letra, i) => {
+         setTimeout (() => 
+            elemento.innerHTML += letra, 100 * i)
+    })
+}
+digitando(titulo);
+
+
+function downloadArquivo() {
+    const link = document.querySelector('a.home-btn')
+    const pdfFile = '../document/Resumo-LázaroG.pdf'
+    const fileName = 'Resumo-LázaroG'
+
+    link.href = pdfFile
+    link.download = fileName
+}
+
